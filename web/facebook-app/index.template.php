ddbb84a94f96e44692d1c0f773a5cddfb25f68e0
sodercober <sodercober@gmail.com>
2011-11-24 09:43:30 +0100

    Add code for embedding Flash

--- web/facebook-app/index.template.php
+++ web/facebook-app/index.template.php
@@ -1,106 +1,139 @@
-<?php
+<?php require_once 'lib/connect.php'; ?>
 
-require 'config.php';
-require 'lib/facebook-sdk/facebook.php';
-require 'lib/oauth2/GrantType/IGrantType.php';
-require 'lib/oauth2/GrantType/ClientCredentials.php';
-require 'lib/oauth2/Client.php';
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!-- saved from url=(0014)about:internet -->
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+    <!--
+    Smart developers always View Source.
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    This application was built using Adobe Flex, an open source framework
+    for building rich Internet applications that get delivered via the
+    Flash Player or to desktops via Adobe AIR.
 
-if (!defined('UNIPASS_URL'))      define('UNIPASS_URL',      'https://www.stworzonedlazdrowia.pl');
-if (!defined('UNIPASS_API_PATH')) define('UNIPASS_API_PATH', '/api/1');
+    Learn more about Flex at http://flex.org
+    // -->
+    <head>
+        <title>${title}</title>
+        <meta name="google" value="notranslate" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <!-- Include CSS to eliminate any default margins/padding and set the height of the html element and
+             the body element to 100%, because Firefox, or any Gecko based browser, interprets percentage as
+             the percentage of the height of its parent container, which has to be set explicitly.  Fix for
+             Firefox 3.6 focus border issues.  Initially, don't display flashContent div so it won't show
+             if JavaScript disabled.
+        -->
+        <style type="text/css" media="screen">
+            html, body  { height:100%; }
+            body { margin:0; padding:0; overflow:auto; text-align:center;
+                   background-color: ${bgcolor}; }
+            object:focus { outline:none; }
+            #flashContent { display:none; }
+        </style>
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        <!-- Enable Browser History by replacing useBrowserHistory tokens with two hyphens -->
+        <!-- BEGIN Browser History required section ${useBrowserHistory}>
+        <link rel="stylesheet" type="text/css" href="history/history.css" />
+        <script type="text/javascript" src="history/history.js"></script>
+        <!${useBrowserHistory} END Browser History required section -->
 
-// Setup Facebook client
-$facebook = new Facebook(array('appId' => FACEBOOK_APP_ID, 'secret' => FACEBOOK_APP_SECRET));
+        <script type="text/javascript" src="swfobject.js"></script>
+        <script type="text/javascript">
+            // For version detection, set to min. required Flash Player version, or 0 (or 0.0.0), for no version detection.
+            var swfVersionStr = "${version_major}.${version_minor}.${version_revision}";
+            // To use express install, set to playerProductInstall.swf, otherwise the empty string.
+            var xiSwfUrlStr = "${expressInstallSwf}";
+            var flashvars = {};
+            flashvars.facebookToken = "<?= $facebook->getAccessToken() ?>";
+            flashvars.unipassToken = "<?= $unipass_token ?>";
+            var params = {};
+            params.quality = "high";
+            params.bgcolor = "${bgcolor}";
+            params.allowscriptaccess = "sameDomain";
+            params.allowfullscreen = "true";
+            var attributes = {};
+            attributes.id = "${application}";
+            attributes.name = "${application}";
+            attributes.align = "middle";
+            swfobject.embedSWF(
+                "${swf}.swf", "flashContent",
+                "${width}", "${height}",
+                swfVersionStr, xiSwfUrlStr,
+                flashvars, params, attributes);
+            // JavaScript enabled so display the flashContent div in case it is not replaced with a swf object.
+            swfobject.createCSS("#flashContent", "display:block;text-align:left;");
+        </script>
+    </head>
+    <body>
+        <div id="fb-root"></div>
+        <!-- SWFObject's dynamic embed method replaces this alternative HTML content with Flash content when enough
+             JavaScript and Flash plug-in support is available. The div is initially hidden so that it doesn't show
+             when JavaScript is disabled.
+        -->
+        <div id="flashContent">
+            <p>
+                To view this page ensure that Adobe Flash Player version
+                ${version_major}.${version_minor}.${version_revision} or greater is installed.
+            </p>
+            <script type="text/javascript">
+                var pageHost = ((document.location.protocol == "https:") ? "https://" : "http://");
+                document.write("<a href='http://www.adobe.com/go/getflashplayer'><img src='"
+                                + pageHost + "www.adobe.com/images/shared/download_buttons/get_flash_player.gif' alt='Get Adobe Flash player' /></a>" );
+            </script>
+        </div>
 
-// Setup Unipass client
-$unipass = new OAuth2\Client(UNIPASS_CLIENT_ID, UNIPASS_CLIENT_SECRET);
-$unipass->setAccessTokenType(OAuth2\Client::ACCESS_TOKEN_BEARER);
-$unipass->setAccessTokenParamName('oauth_token');
+        <noscript>
+            <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="${width}" height="${height}" id="${application}">
+                <param name="movie" value="${swf}.swf" />
+                <param name="quality" value="high" />
+                <param name="bgcolor" value="${bgcolor}" />
+                <param name="allowScriptAccess" value="sameDomain" />
+                <param name="allowFullScreen" value="true" />
+                <!--[if !IE]>-->
+                <object type="application/x-shockwave-flash" data="${swf}.swf" width="${width}" height="${height}">
+                    <param name="quality" value="high" />
+                    <param name="bgcolor" value="${bgcolor}" />
+                    <param name="allowScriptAccess" value="sameDomain" />
+                    <param name="allowFullScreen" value="true" />
+                <!--<![endif]-->
+                <!--[if gte IE 6]>-->
+                    <p>
+                        Either scripts and active content are not permitted to run or Adobe Flash Player version
+                        ${version_major}.${version_minor}.${version_revision} or greater is not installed.
+                    </p>
+                <!--<![endif]-->
+                    <a href="http://www.adobe.com/go/getflashplayer">
+                        <img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Get Adobe Flash Player" />
+                    </a>
+                <!--[if !IE]>-->
+                </object>
+                <!--<![endif]-->
+            </object>
+        </noscript>
 
-// See if there is a user from a cookie
-$facebook_user = $facebook->getUser();
+        <script type="text/javascript">
+            window.fbAsyncInit = function() {
+                FB.init({
+                    appId  : '<?php echo $facebook->getAppID() ?>',
+                    status : true,
+                    cookie : true,
+                    xfbml  : true,
+                    oauth  : true
+                });
 
-if ($facebook_user) {
-    try {
-        $facebook_profile = $facebook->api('/me');
+                FB.Event.subscribe('auth.login', function(response) {
+                    window.location.reload();
+                });
 
-        // Fetch client access token from Unipass
-        $response = $unipass->getAccessToken(UNIPASS_URL.'/oauth2/token', OAuth2\Client::GRANT_TYPE_CLIENT_CREDENTIALS, array());
-        $unipass_client_token = $response['result']['access_token'];
-        $unipass->setAccessToken($unipass_client_token);
-
-        // Automatically join validated Facebook account with Unipass account (by Facebook UID / User e-mail)
-        $response = $unipass->fetch(UNIPASS_URL.UNIPASS_API_PATH.'/users/connect',
-                                    array(
-                                         'provider' => 'facebook',
-                                         'uid'      => $facebook_profile['id'],
-                                         'email'    => $facebook_profile['email']
-                                    ),
-                                    OAuth2\Client::HTTP_METHOD_POST);
-
-        // Unipass auto-connect returns user's access_token
-        $unipass_token = $response['result']['access_token'];
-        $unipass->setAccessToken($unipass_token);
-
-        $response = $unipass->fetch(UNIPASS_URL.UNIPASS_API_PATH.'/me');
-        $unipass_profile = $response['result'];
-    } catch (Exception $e) {
-        echo('<pre>' . htmlspecialchars(print_r($e, true)) . '</pre>');
-        $facebook_user = null;
-        $unipass_token = null;
-    }
-} else {
-    $url = $facebook->getLoginUrl(array('scope' => FACEBOOK_SCOPE, 'redirect_uri' => FACEBOOK_APP_URL));
-    echo("<script type='text/javascript'>top.location.href = '$url';</script>");
-}
-
-?>
-
-<!DOCTYPE html>
-<html xmlns:fb="http://www.facebook.com/2008/fbml">
-<body>
-<?php if ($facebook_profile) { ?>
-    Your Facebook profile is
-    <pre><?php print htmlspecialchars(print_r($facebook_profile, true)) ?></pre>
-<?php } else { ?>
-    <fb:login-button></fb:login-button>
-<?php } ?>
-
-<?php if ($unipass_token) { ?>
-    Your Unipass profile is
-    <pre><?php print htmlspecialchars(print_r($unipass_profile, true)) ?></pre>
-<?php } else { ?>
-    No Unipass data
-<?php } ?>
-
-<div id="fb-root"></div>
-<script>
-    window.fbAsyncInit = function() {
-        FB.init({
-            appId  : '<?php echo $facebook->getAppID() ?>',
-            status : true,
-            cookie : true,
-            xfbml  : true,
-            oauth  : true
-        });
-
-        FB.Event.subscribe('auth.login', function(response) {
-            window.location.reload();
-        });
-        FB.Event.subscribe('auth.logout', function(response) {
-            window.location.reload();
-        });
-    };
-    (function() {
-        var e = document.createElement('script');
-        e.async = true;
-        e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
-        document.getElementById('fb-root').appendChild(e);
-    }());
-</script>
-</body>
+                FB.Event.subscribe('auth.logout', function(response) {
+                    window.location.reload();
+                });
+            };
+            (function() {
+                var e = document.createElement('script');
+                e.async = true;
+                e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
+                document.getElementById('fb-root').appendChild(e);
+            }());
+        </script>
+   </body>
 </html>
---
+++ web/facebook-app/lib/connect.php
@@ -0,0 +1,61 @@
+<?php
+
+require_once '../config.php';
+require_once 'facebook-sdk/facebook.php';
+require_once 'oauth2/GrantType/IGrantType.php';
+require_once 'oauth2/GrantType/ClientCredentials.php';
+require_once 'oauth2/Client.php';
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+!defined('UNIPASS_URL')      && define('UNIPASS_URL',      'https://www.stworzonedlazdrowia.pl');
+!defined('UNIPASS_API_PATH') && define('UNIPASS_API_PATH', '/api/1');
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Variables to be used by HTML template
+global $facebook;
+global $unipass_token;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Setup Facebook client
+$facebook = new Facebook(array('appId' => FACEBOOK_APP_ID, 'secret' => FACEBOOK_APP_SECRET));
+
+// Setup Unipass client
+$unipass = new OAuth2\Client(UNIPASS_CLIENT_ID, UNIPASS_CLIENT_SECRET);
+$unipass->setAccessTokenType(OAuth2\Client::ACCESS_TOKEN_BEARER);
+$unipass->setAccessTokenParamName('oauth_token');
+
+// See if there is a Facebook user from a cookie
+$facebook_user = $facebook->getUser();
+
+if ($facebook_user) {
+    try {
+        $facebook_profile = $facebook->api('/me');
+
+        // Fetch client access token from Unipass
+        $response = $unipass->getAccessToken(UNIPASS_URL.'/oauth2/token', OAuth2\Client::GRANT_TYPE_CLIENT_CREDENTIALS, array());
+        $unipass_client_token = $response['result']['access_token'];
+        $unipass->setAccessToken($unipass_client_token);
+
+        // Automatically join validated Facebook account with Unipass account (by Facebook UID / Facebook user e-mail)
+        $response = $unipass->fetch(UNIPASS_URL.UNIPASS_API_PATH.'/users/connect',
+                                    array(
+                                         'provider' => 'facebook',
+                                         'uid'      => $facebook_profile['id'],
+                                         'email'    => $facebook_profile['email']
+                                    ),
+                                    OAuth2\Client::HTTP_METHOD_POST);
+
+        // Unipass auto-connect returns user's access_token
+        $unipass_token = $response['result']['access_token'];
+        $unipass->setAccessToken($unipass_token);
+    } catch (Exception $e) {
+        echo('<pre>'.htmlspecialchars(print_r($e, true)).'</pre>');
+        $facebook = $unipass_token = null;
+    }
+} else {
+    $url = $facebook->getLoginUrl(array('scope' => FACEBOOK_SCOPE, 'redirect_uri' => FACEBOOK_APP_URL));
+    echo("<script type='text/javascript'>top.location.href = '$url';</script>");
+}
