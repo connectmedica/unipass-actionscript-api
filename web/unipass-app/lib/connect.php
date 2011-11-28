3e35505373d0f8158955a3d2cdbbdc8ef07f843c
sodercober <sodercober@gmail.com>
2011-11-28 19:18:29 +0100

    Add web template for flash embedding

---
+++ web/unipass-app/lib/connect.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once 'oauth2/GrantType/IGrantType.php';
+require_once 'oauth2/GrantType/AuthorizationCode.php';
+require_once 'oauth2/Client.php';
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+!defined('UNIPASS_URL')            && define('UNIPASS_URL',            'https://www.stworzonedlazdrowia.pl');
+!defined('UNIPASS_AUTH_ENDPOINT')  && define('UNIPASS_AUTH_ENDPOINT',  '/oauth2/authorize');
+!defined('UNIPASS_TOKEN_ENDPOINT') && define('UNIPASS_TOKEN_ENDPOINT', '/oauth2/token');
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Variables to be used by HTML template
+global $unipass_token;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Setup Unipass client
+$unipass = new OAuth2\Client(UNIPASS_CLIENT_ID, UNIPASS_CLIENT_SECRET);
+$unipass->setAccessTokenType(OAuth2\Client::ACCESS_TOKEN_BEARER);
+$unipass->setAccessTokenParamName('oauth_token');
+
+if (!isset($_GET['code'])) {
+    $auth_url = $client->getAuthenticationUrl(UNIPASS_URL.UNIPASS_AUTH_ENDPOINT, REDIRECT_URI);
+    header('Location: '.$auth_url);
+    die('Redirect');
+} else {
+    $params = array('code' => $_GET['code'], 'redirect_uri' => REDIRECT_URI);
+    $response = $client->getAccessToken(UNIPASS_URL.UNIPASS_TOKEN_ENDPOINT, 'authorization_code', $params);
+    parse_str($response['result'], $info);
+    $unipass_token = $info['access_token'];
+    $unipass->setAccessToken($unipass_token);
+}
